559b0b13eabc9c2f5567b87c6f430c99414a5ff3
kalmarek <kalmar@amu.edu.pl>
2018-10-20 23:17:51 +0200

    add tests for lattice points
    LATTICE_POINTS_GENERATORS is a pm::Array<pm::Matrix<pm::Integer>>
    
    FAR_FACE is a pm::Set<int32_t>

--- test/perlobj.jl
+++ test/perlobj.jl
@@ -1,6 +1,7 @@
 @testset "perlobj" begin
     input_dict_int = Dict( "POINTS" => [ 1 0 0 ; 1 3 0 ; 1 0 3 ; 1 3 3 ] )
     input_dict_rat = Dict( "POINTS" => Array{Rational{Int64},2}([ 1 0 0 ; 1 3 0 ; 1 0 3 ; 1 3 3 ]) )
+    input_dict_unbounded = Dict("POINTS" => [1 0 0; 0 1 1])
 
     @testset "constructors" begin
         @test perlobj("Polytope", input_dict_int ) isa pm_perl_Object
@@ -16,5 +17,12 @@
         @test test_polytope.F_VECTOR == [ 4, 4 ]
         @test test_polytope.INTERIOR_LATTICE_POINTS ==
             [ 1 1 1 ; 1 1 2 ; 1 2 1 ; 1 2 2 ]
+    end
+    
+    @testset "lattice points" begin
+        test_polytope = perlobj("Polytope", input_dict_unbounded )
+        @test test_polytope.LATTICE_POINTS_GENERATORS ==
+            [[ 1 0 0;], [ 0 1 1;], Matrix{Int}(undef, 0,3)]
+        @test test_polytope.FAR_FACE == Set([1])
     end
 end
