fd511d81ac037b226628a666fbc826df90446351
Benjamin Lorenz <lorenz@math.tu-berlin.de>
2018-10-30 16:27:22 +0100

    minor cleanup

--- src/object_helpers.jl
+++ src/object_helpers.jl
@@ -1,10 +1,10 @@
 
 function complete_property(obj::pm_perl_Object,prefix::String)
-   return convert_from_property_value(Polymake.call_function("complete_property",Array{Any,1}([obj,prefix])))
+   return convert_from_property_value(call_function("complete_property",Array{Any,1}([obj,prefix])))
 end
 
 function list_applications()
-   return convert_from_property_value(Polymake.call_function("list_applications",Array{Any,1}([])))
+   return convert_from_property_value(call_function("list_applications",Array{Any,1}([])))
 end
 
 function list_big_objects(app::String)
