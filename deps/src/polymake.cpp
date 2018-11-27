ed7523e7ee37ea34b971a1498ac0ea1a0135ff96
Olivia Röhrig <unipost@posteo.de>
2018-11-27 15:52:55 +0100

    wrapped vector in seperate file

--- deps/src/polymake.cpp
+++ deps/src/polymake.cpp
@@ -12,6 +12,8 @@
 
 #include "polymake_matrices.h"
 
+#include "polymake_vectors.h"
+
 #include "polymake_arrays.h"
 
 #include "polymake_caller.h"
@@ -39,39 +41,7 @@ JLCXX_MODULE define_module_polymake(jlcxx::Module& polymake)
 
   polymake_module_add_matrix(polymake);
 
-  polymake.add_type<jlcxx::Parametric<jlcxx::TypeVar<1>>>("pm_Vector", jlcxx::julia_type("AbstractVector", "Base"))
-    .apply<
-      pm::Vector<pm::Integer>,
-      pm::Vector<pm::Rational>
-    >([](auto wrapped){
-        typedef typename decltype(wrapped)::type WrappedT;
-        typedef typename decltype(wrapped)::type::value_type elemType;
-        wrapped.template constructor<int32_t>();
-        wrapped.template constructor<int64_t>();
-        wrapped.method("_getindex", [](WrappedT& V, int64_t n){
-          return elemType(V[n-1]);
-        });
-        wrapped.method("_setindex!",[](WrappedT& V, elemType val, int64_t n){
-            V[n-1]=val;
-        });
-        wrapped.method("length", &WrappedT::size);
-        wrapped.method("resize!",[](WrappedT& V, int64_t sz){
-          V.resize(sz);
-        });
-
-        wrapped.method("take",[](pm::perl::Object p, const std::string& s, WrappedT& V){
-            p.take(s) << V;
-        });
-        wrapped.method("show_small_obj", [](WrappedT& V){
-          return show_small_object<WrappedT>(V);
-        });
-    });
-  polymake.method("to_vector_Integer", [](pm::perl::PropertyValue pv){
-    return to_SmallObject<pm::Vector<pm::Integer>>(pv);
-  });
-  polymake.method("to_vector_Rational", [](pm::perl::PropertyValue pv){
-    return to_SmallObject<pm::Vector<pm::Rational>>(pv);
-  });
+  polymake_module_add_vector(polymake);
 
   polymake.method("initialize_polymake", &initialize_polymake);
   polymake.method("application",[](const std::string x){
