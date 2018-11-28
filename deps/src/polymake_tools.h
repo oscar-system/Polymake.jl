b01f7f7cdf26fdc1e6250ef8cb17f795db0c85d7
Sascha Timme <sascha.timme@gmail.com>
2018-11-28 17:53:25 +0100

    Run clang-format

--- deps/src/polymake_tools.h
+++ deps/src/polymake_tools.h
@@ -89,8 +89,9 @@ static inline void* get_ptr_from_cxxwrap_obj(jl_value_t* obj)
     return *reinterpret_cast<void**>(obj);
 }
 
-    // void* get_ptr_from_cxxwrap_obj(jl_value_t* obj){
-    //     return jl_unbox_voidpointer(jl_get_field(obj,"cpp_object"));
-    // }
+// Safe way
+// void* get_ptr_from_cxxwrap_obj(jl_value_t* obj){
+//     return jl_unbox_voidpointer(jl_get_field(obj,"cpp_object"));
+// }
 
 #endif
