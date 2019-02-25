237913d91b459d22ffd27951491cb35c412effab
kalmarek <kalmar@amu.edu.pl>
2019-02-25 16:46:37 +0100

    more tests
    * template parameters
    * from polymake tutorials

--- test/perlobj.jl
+++ test/perlobj.jl
@@ -12,7 +12,11 @@
             :POINTS => [ 1 0 0 ; 1 3 0 ; 1 0 3 ; 1 3 3 ]) isa pm_perl_Object
 
         @test (@pm Polytopes.Polytope(input_dict_int)) isa pm_perl_Object
+        @test (@pm Polytopes.Polytope{Rational}(input_dict_int)) isa pm_perl_Object
+        @test (@pm Polytopes.Polytope{QuadraticExtension}(input_dict_int)) isa pm_perl_Object
+
         @test (@pm Polytopes.Polytope(input_dict_rat)) isa pm_perl_Object
+
         @test (@pm Polytopes.Polytope(POINTS=[ 1 0 0 ; 1 3 0 ; 1 0 3 ; 1 3 3 ])) isa pm_perl_Object
         @test (@pm Polytopes.Polytope(:POINTS=>[ 1 0 0 ; 1 3 0 ; 1 0 3 ; 1 3 3 ])) isa pm_perl_Object
         @test (@pm Polytopes.Polytope("POINTS"=>[ 1 0 0 ; 1 3 0 ; 1 0 3 ; 1 3 3 ])) isa pm_perl_Object
@@ -21,6 +25,7 @@
 
         @test (@pm Tropical.Polytope{Max}(input_dict_int)) isa pm_perl_Object
         @test (@pm Tropical.Polytope{Max, Rational}(input_dict_int)) isa pm_perl_Object
+        @test (@pm Tropical.Polytope{Max, QuadraticExtension}(input_dict_int)) isa pm_perl_Object
     end
 
     @testset "PolymakeException" begin
@@ -69,5 +74,49 @@
             @test loaded isa pm_perl_Object
             @test Base.propertynames(test_polytope) == Base.propertynames(loaded)
         end
+    end
+
+    @testset "polymake tutorials" begin
+        p = @pm Polytopes.Polytope(:POINTS=>Polymake.Polytopes.cube(4).VERTICES)
+        @test p isa pm_perl_Object
+
+        lp = @pm Polytopes.LinearProgram(:LINEAR_OBJECTIVE=>[0,1,1,1,1])
+        @test lp isa pm_perl_Object
+
+        @test (p.LP = lp) isa pm_perl_Object
+        @test p.LP.MAXIMAL_VALUE == 4
+
+        matrix = Rational{Int64}[
+            1//1  0//1  0//1  0//1;
+            1//1  1//16 1//4  1//16;
+            1//1  3//8  1//4  1//32;
+            1//1  1//4  3//8  1//32;
+            1//1  1//16 1//16 1//4;
+            1//1  1//32 3//8  1//4;
+            1//1  1//4  1//16 1//16;
+            1//1  1//32 1//4  3//8;
+            1//1  3//8  1//32 1//4;
+            1//1  1//4  1//32 3//8]
+
+        special_points = pm_Rational[
+            1 1//16 1//4 1//16;
+            1 1//16 1//16 1//4;
+            1 1//4 1//16 1//16]
+
+        p = @pm Polytopes.Polytope(:POINTS=>matrix)
+
+        @test Polymake.Polytopes.DIM(p) == 3
+
+        @test p.VERTEX_SIZES == [9, 3, 4, 4, 3, 4, 3, 4, 4, 4]
+
+        s = Set(i for (i, vsize) in enumerate(p.VERTEX_SIZES)
+                if vsize == Polymake.Polytopes.DIM(p))
+        pm_s = pm_Set(i for (i, vsize) in enumerate(p.VERTEX_SIZES)
+                if vsize == Polymake.Polytopes.DIM(p))
+
+        @test Set([2,5,7]) == s == pm_s
+
+        @test p.VERTICES[collect(pm_s), :] isa Matrix{pm_Rational}
+        @test p.VERTICES[collect(pm_s), :] == special_points
     end
 end
