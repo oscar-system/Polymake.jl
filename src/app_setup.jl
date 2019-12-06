24d6d356899e2e87138f3f3c7e0ba255096a0bfa
kalmarek <kalmar@amu.edu.pl>
2019-12-06 07:04:08 +0100

    pluralize the App names that conflict with names of types

--- src/app_setup.jl
+++ src/app_setup.jl
@@ -1,14 +1,14 @@
 const appname_module_dict = Dict(
-  :common  => :Common,
-  :fan  => :Fan,
-  :fulton  => :Fulton,
-  :graph  => :Graph,
-  :group  => :Group,
-  :ideal  => :Ideal,
-  :matroid  => :Matroid,
-  :polytope  => :Polytope,
-  :topaz  => :Topaz,
-  :tropical  => :Tropical
+  :common     => :Common,
+  :fan        => :Fans,
+  :fulton     => :Fulton,
+  :graph      => :Graphs,
+  :group      => :Groups,
+  :ideal      => :Ideals,
+  :matroid    => :Matroids,
+  :polytope   => :Polytopes,
+  :topaz      => :Topaz,
+  :tropical   => :Tropical
 )
 
 const module_appname_dict = Dict( (j,i) for (i,j) in appname_module_dict )
