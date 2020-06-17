44624f1f04e599d0c6fbfc93565dda80c1aa8419
Benjamin Lorenz <lorenz@math.tu-berlin.de>
2020-06-17 22:03:33 +0200

    for cxxwrap 0.11: both copy and deepcopy should do a C++ copy

--- src/Polymake.jl
+++ src/Polymake.jl
@@ -42,7 +42,7 @@ function Base.showerror(io::IO, ex::PolymakeError)
 end
 
 function check_jlcxx_version(version)
-    current_jlcxx = VersionNumber(unsafe_string(ccall(:cxxwrap_version_string, Cstring, ())))
+    global current_jlcxx = VersionNumber(unsafe_string(ccall(:cxxwrap_version_string, Cstring, ())))
     if (version != current_jlcxx)
         error("""JlCxx version changed, please run `using Pkg; Pkg.build("Polymake");`""")
     end
