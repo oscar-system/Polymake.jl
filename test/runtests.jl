2632470e1a030b023db22a91d62bb41094df1977
Alexej Jordan <jordan@math.tu-berlin.de>
2020-06-23 17:28:50 +0200

    test workflow

--- test/runtests.jl
+++ test/runtests.jl
@@ -26,7 +26,7 @@ struct MyInt x::Int end # needed in test/convert.jl
     include("polynomial.jl")
     include("pairs.jl")
     include("lists.jl")
-    if ENV["JULIA_PDB_TEST"]
+    if isdefined(:PDB_TEST)
         @testset "Polymake.Polydb" begin
             @test 1 == 1
         end
