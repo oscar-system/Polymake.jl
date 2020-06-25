143904f069775e4ba22d273716e3cd0b8379972f
Alexej Jordan <jordan@math.tu-berlin.de>
2020-06-25 03:41:28 +0200

    test testing (iterator)

--- test/polydb.jl
+++ test/polydb.jl
@@ -28,16 +28,16 @@ Polymake.Polydb._set_uri(get(ENV, "POLYDB_SERVER_URI", ""))
         @test Polymake.Polydb.find(collection_bson, query) isa Polymake.Polydb.Cursor{Mongoc.BSON}
         @test Polymake.Polydb.find(collection_bson, constraints...) isa Polymake.Polydb.Cursor
         @test Polymake.Polydb.find(collection_bson, constraints...) isa Polymake.Polydb.Cursor{Mongoc.BSON}
-        # @testset "Iterator (Cursor)" begin
-        #     results_bo = Polymake.Polydb.find(collection_bo, constraints...)
-        #     results_bson = Polymake.Polydb.find(collection_bson, constraints...)
-        #     @test iterate(results_bo) isa Tuple{Polymake.BigObject, Nothing}
-        #     @test iterate(results_bson) isa Tuple{Mongoc.BSON, Nothing}
-        #     results_bo = Polymake.Polydb.find(collection_bo, constraints...)
-        #     results_bson = Polymake.Polydb.find(collection_bson, constraints...)
-        #     @test collect(results_bo) isa Array{Polymake.BigObject, 1}
-        #     @test collect(results_bson) isa Array{Mongoc.BSON, 1}
-        # end
+        @testset "Iterator (Cursor)" begin
+            results_bo = Polymake.Polydb.find(collection_bo, constraints...)
+            results_bson = Polymake.Polydb.find(collection_bson, constraints...)
+            @test iterate(results_bo) isa Tuple{Polymake.BigObject, Nothing}
+            @test iterate(results_bson) isa Tuple{Mongoc.BSON, Nothing}
+            results_bo = Polymake.Polydb.find(collection_bo, constraints...)
+            results_bson = Polymake.Polydb.find(collection_bson, constraints...)
+            @test collect(results_bo) isa Array{Polymake.BigObject, 1}
+            @test collect(results_bson) isa Array{Mongoc.BSON, 1}
+        end
         # @testset "Iterator (Collection)" begin
         #     @test iterate(collection_bo) isa Tuple{Polymake.BigObject, Polymake.Polydb.Cursor{Polymake.BigObject}}
         #     @test iterate(collection_bson) isa Tuple{Mongoc.BSON, Mongoc.Cursor}
