bfcf6d206973e41c611ff88eb8cc3e86f9a4f4a0
Alexej Jordan <jordan@math.tu-berlin.de>
2020-06-25 02:13:47 +0200

    test testing

--- test/polydb.jl
+++ test/polydb.jl
@@ -23,41 +23,41 @@ Polymake.Polydb._set_uri(get(ENV, "POLYDB_SERVER_URI", ""))
         @test Polymake.Polydb.find(collection_bson, query) isa Polymake.Polydb.Cursor{Mongoc.BSON}
         @test Polymake.Polydb.find(collection_bson, constraints...) isa Polymake.Polydb.Cursor
         @test Polymake.Polydb.find(collection_bson, constraints...) isa Polymake.Polydb.Cursor{Mongoc.BSON}
-        # @testset "Iterator (Cursor)" begin
-        #     results_bo = Polymake.Polydb.find(collection_bo, constraints...)
-        #     results_bson = Polymake.Polydb.find(collection_bson, constraints...)
-        #     @test iterate(results_bo) isa Tuple{Polymake.BigObject, Nothing}
-        #     @test iterate(results_bson) isa Tuple{Mongoc.BSON, Nothing}
-        #     results_bo = Polymake.Polydb.find(collection_bo, constraints...)
-        #     results_bson = Polymake.Polydb.find(collection_bson, constraints...)
-        #     @test collect(results_bo) isa Array{Polymake.BigObject, 1}
-        #     @test collect(results_bson) isa Array{Mongoc.BSON, 1}
-        # end
-        # @testset "Iterator (Collection)" begin
-        #     @test iterate(collection_bo) isa Tuple{Polymake.BigObject, Polymake.Polydb.Cursor{Polymake.BigObject}}
-        #     @test iterate(collection_bson) isa Tuple{Mongoc.BSON, Mongoc.Cursor}
-        #     @test collect(collection_bo) isa Array{Polymake.BigObject}
-        #     @test collect(collection_bson) isa Array{Mongoc.BSON}
-        # end
-        # @testset "Information" begin
-        #     @test Polymake.Polydb.get_fields(collection_bo) isa Array{String, 1}
-        #     fields = Polymake.Polydb.get_fields(collection_bo)
-        #     @test length(fields) == 44
-        #     @test fields[1] == "AFFINE_HULL"
-        # end
+        @testset "Iterator (Cursor)" begin
+            results_bo = Polymake.Polydb.find(collection_bo, constraints...)
+            results_bson = Polymake.Polydb.find(collection_bson, constraints...)
+            @test iterate(results_bo) isa Tuple{Polymake.BigObject, Nothing}
+            @test iterate(results_bson) isa Tuple{Mongoc.BSON, Nothing}
+            results_bo = Polymake.Polydb.find(collection_bo, constraints...)
+            results_bson = Polymake.Polydb.find(collection_bson, constraints...)
+            @test collect(results_bo) isa Array{Polymake.BigObject, 1}
+            @test collect(results_bson) isa Array{Mongoc.BSON, 1}
+        end
+        @testset "Iterator (Collection)" begin
+            @test iterate(collection_bo) isa Tuple{Polymake.BigObject, Polymake.Polydb.Cursor{Polymake.BigObject}}
+            @test iterate(collection_bson) isa Tuple{Mongoc.BSON, Mongoc.Cursor}
+            @test collect(collection_bo) isa Array{Polymake.BigObject}
+            @test collect(collection_bson) isa Array{Mongoc.BSON}
+        end
+        @testset "Information" begin
+            @test Polymake.Polydb.get_fields(collection_bo) isa Array{String, 1}
+            fields = Polymake.Polydb.get_fields(collection_bo)
+            @test length(fields) == 44
+            @test fields[1] == "AFFINE_HULL"
+        end
     end
 
-    # @testset "Basic querying" begin
-    #     db = Polymake.Polydb.get_db()
-    #     collection_bo = db["Polytopes.Lattice.SmoothReflexive"]
-    #     @testset "`Polymake.BigObject`-templated types" begin
-    #         complete = collect(collection_bo)
-    #         @test length(complete) == 25
-    #         constraints = ["N_VERTICES" => 8]
-    #         query = Dict(constraints...)
-    #         results = collect(Polymake.Polydb.find(collection, constraints...))
-    #     end
-    # end
+    @testset "Basic querying" begin
+        db = Polymake.Polydb.get_db()
+        collection_bo = db["Polytopes.Lattice.SmoothReflexive"]
+        @testset "`Polymake.BigObject`-templated types" begin
+            complete = collect(collection_bo)
+            @test length(complete) == 25
+            constraints = ["N_VERTICES" => 8]
+            query = Dict(constraints...)
+            results = collect(Polymake.Polydb.find(collection, constraints...))
+        end
+    end
     #
     # @testset "Query macros" begin
     #     @test 1 == 1
