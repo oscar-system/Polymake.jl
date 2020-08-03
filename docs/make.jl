f8194a3fb5a6ddd0f36023d1d58eda274a2fc9e3
Alexej Jordan <jordan@math.tu-berlin.de>
2020-08-04 00:38:43 +0200

    adjusted workflow for building and deployment

--- docs/make.jl
+++ docs/make.jl
@@ -1,3 +1,7 @@
 using Documenter, Polymake
 
 makedocs(sitename = "Polymake.jl - Documentation", pages = ["index.md", "getting_started.md", "using_polymake_jl.md", "examples.md"])
+
+deploydocs(
+    repo = "github.com/oscar-system/Polymake.jl.git",
+)
