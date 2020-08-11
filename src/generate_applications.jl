6bea8dba1d5350c66fa597676057b13a5b188292
Benjamin Lorenz <lorenz@math.tu-berlin.de>
2020-08-11 21:42:06 +0200

    remove duplicate json folder variable

--- src/generate_applications.jl
+++ src/generate_applications.jl
@@ -4,12 +4,8 @@ include("meta.jl")
 
 using Polymake.Meta
 
-generated_dir = joinpath(@__DIR__, "generated")
-json_dir = joinpath(generated_dir, "json")
-isdir(generated_dir) || mkpath(generated_dir)
-
 for (app, mod) in appname_module_dict
-    json_file = joinpath(json_dir, "$app.json")
+    json_file = joinpath(json_folder, "$app.json")
     @assert isfile(json_file)
     @info "Generating module $mod"
     @eval $(Polymake.Meta.jl_code(Polymake.Meta.PolymakeApp(mod, json_file)))
