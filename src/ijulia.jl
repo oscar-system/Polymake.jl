f731e88abb4eeef55b7a2f9a963e3a983d9d031f
Benjamin Lorenz <lorenz@math.tu-berlin.de>
2021-01-05 00:30:35 +0100

    ijulia: fetch threejs resources directly from polymake_jll
    move them to a subdirectory of the julia kernel
    remove outdated threejs files

--- src/ijulia.jl
+++ src/ijulia.jl
@@ -1,10 +1,15 @@
 import JSON
 
 function copy_javascript_files(target_dir)
-    jupyter_resources = joinpath(@__DIR__, "..", "resources","jupyter")
+    jupyter_resources = joinpath(polymake_jll.artifact_dir, "share",
+                                 "polymake", "resources", "jupyter-polymake",
+                                 "jupyter_kernel_polymake", "resources")
+    mkpath(target_dir)
     for file in readdir(jupyter_resources)
-        cp(joinpath(jupyter_resources, file),
-           joinpath(target_dir, file); force=true)
+        if endswith(file,".svg") || endswith(file,".js") && file != "kernel.js"
+            cp(joinpath(jupyter_resources, file),
+               joinpath(target_dir, file); force=true)
+        end
     end
     nothing
 end
@@ -23,7 +28,7 @@ function prepare_jupyter_kernel_for_visualization()
 
     for (kernel, spec) in kernelspecs
         if occursin("julia", kernel)
-            copy_javascript_files(spec["resource_dir"])
+            copy_javascript_files(joinpath(spec["resource_dir"],"polymake"))
         end
     end
     nothing
