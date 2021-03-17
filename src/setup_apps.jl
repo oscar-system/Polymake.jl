6aa72f492e30bb557a5d9ee256ca522277d23820
Benjamin Lorenz <lorenz@math.tu-berlin.de>
2021-03-17 01:29:48 +0100

    prefer labels do syntax (via scope), add bigobject copy, tests

--- src/setup_apps.jl
+++ src/setup_apps.jl
@@ -16,3 +16,6 @@ const module_appname_dict = Dict( (j,i) for (i,j) in appname_module_dict )
 list_applications() = call_function(:common, :list_applications)
 
 list_big_objects(app::Symbol) = call_function(:common, :list_big_objects, string(app))
+
+
+list_labels(app::Symbol) = call_function(:common, :list_labels, string(app))
--- src/util.jl
+++ src/util.jl
@@ -41,8 +41,33 @@ function cite(;format=:bibtex)
     end
 end
 
+labeldict = Dict{String,Base.Array{String}}()
+
+function lookup_label_app(label_expression::String)
+   if isempty(labeldict)
+      for app in list_applications()
+         for label in list_labels(Symbol(app))
+            if haskey(labeldict,label)
+               push!(labeldict[label],app)
+            else
+               labeldict[label] = [app]
+            end
+         end
+      end
+   end
+   label = split(label_expression,".",limit=2)[1]
+   apps = get(labeldict,label,[])
+   if length(apps) == 1
+      return apps[1]
+   elseif length(apps) == 0
+      throw(PolymakeError("label '$label' not found"))
+   else
+      throw(PolymakeError("label '$label' ambiguous: applications $apps"))
+   end
+end
+
 """
-    prefer(label_expression::String)
+    prefer(f::Function, label_expression::String; application::String="")
 
 Make the production rules, user functions, or methods (further called items) matching the given `label_expression` be preferred over competing rules (functions, methods).
 
@@ -50,12 +75,43 @@ Make the production rules, user functions, or methods (further called items) mat
 
 If you prefer to use lrs as the default for the convex hull computation set
 ```
-prefer("lrs.convex_hull")
+c = polytope.cube(3)
+prefer("lrs.convex_hull") do
+   c.VERTICES
+end
 ```
 
 Another example
 ```
-prefer("cdd.simplex")
+m = matroid.fano_matroid()
+prefer("_4ti2"; application="matroid") do
+   m.CIRCUITS
+end
 ```
+
+FIXME: this is deprecated in favour of the do block syntax
+       also it doesn't really work as the labels are not looked up
+       in the correct application
 """
-prefer(label_expression::String) = set_preference(label_expression)
+function prefer(label_expression::String)
+    Base.depwarn("`prefer(label)` is deprecated, use `prefer(label) do ... end` instead.", :prefer, force=true)
+   set_preference(label_expression)
+end
+
+function prefer(f::Function, label_expression::String; application::String="")
+   old_app = get_current_app()
+   scope = scope_begin()
+   if application == ""
+      application = lookup_label_app(label_expression)
+   end
+   # switching apps will just switch some references within polymake back and forth
+   # we need to do this for the proper label lookup
+   Polymake.application(application)
+   internal_prefer_now(scope, label_expression)
+   Polymake.application(old_app)
+
+   res = f()
+
+   scope_end(scope)
+   return res
+end
