e4870876684e26b54ed1cc572e221d503a105e3d
Alexej Jordan <jordan@math.tu-berlin.de>
2022-01-12 19:05:06 +0100

    adjusted handling of elements of lists, added tests

--- src/std/lists.jl
+++ src/std/lists.jl
@@ -1,21 +1,23 @@
-Base.eltype(::StdList{StdPair{T, T}}) where T = Pair{T,T}
+Base.eltype(::StdList{StdPair{S, T}}) where {S, T} = Pair{T,T}
 
-function Base.iterate(L::StdList)
+Base.push!(L::StdList{<:StdPair}, a::Pair) = push!(L, StdPair(a))
+
+Base.eltype(::StdList{T}) where T = T
+
+function Base.iterate(L::StdList{T}) where T
     isempty(L) &&  return nothing
     state = beginiterator(L)
     elt = get_element(state)
     increment(state)
-    return Pair(elt), state
+    return elt, state
 end
 
-function Base.iterate(L::StdList, state)
+function Base.iterate(L::StdList{T}, state) where T
     if isdone(L, state)
         return nothing
     else
         elt = get_element(state)
         increment(state)
-        return Pair(elt), state
+        return elt, state
     end
 end
-
-Base.push!(L::StdList{<:StdPair}, a::Pair) = push!(L, StdPair(a))
--- src/std/pairs.jl
+++ src/std/pairs.jl
@@ -11,3 +11,5 @@ Base.eltype(p::StdPair{T,T}) where T = T
 Base.iterate(p::StdPair) = first(p), Val{:first}()
 Base.iterate(p::StdPair, ::Val{:first}) = last(p), Val{:last}()
 Base.iterate(p::StdPair, ::Val{:last}) = nothing
+
+Base.:(==)(p::Union{StdPair{S, T}, Pair{S, T}}, q::Union{StdPair{U, V}, Pair{U, V}}) where {S, T, U, V} = convert(Pair{S, T}, p) == convert(Pair{U, V}, q)
