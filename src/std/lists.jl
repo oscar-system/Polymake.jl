851667df6fc7108984059d7507fd0d1a214735f7
Alexej Jordan <jordan@math.tu-berlin.de>
2022-01-13 12:56:00 +0100

    applied comments from github and fixed Pair conversion

--- src/std/lists.jl
+++ src/std/lists.jl
@@ -1,10 +1,10 @@
-Base.eltype(::StdList{StdPair{S, T}}) where {S, T} = Pair{T,T}
+Base.eltype(::StdList{StdPair{S, T}}) where {S, T} = Pair{S,T}
 
 Base.push!(L::StdList{<:StdPair}, a::Pair) = push!(L, StdPair(a))
 
 Base.eltype(::StdList{T}) where T = T
 
-function Base.iterate(L::StdList{T}) where T
+function Base.iterate(L::StdList)
     isempty(L) &&  return nothing
     state = beginiterator(L)
     elt = get_element(state)
@@ -12,7 +12,7 @@ function Base.iterate(L::StdList{T}) where T
     return elt, state
 end
 
-function Base.iterate(L::StdList{T}, state) where T
+function Base.iterate(L::StdList, state)
     if isdone(L, state)
         return nothing
     else
--- src/std/pairs.jl
+++ src/std/pairs.jl
@@ -4,7 +4,7 @@ StdPair(p::Pair) = StdPair(first(p), last(p))
 Base.Pair(p::StdPair) = Pair(first(p), last(p))
 Base.Pair{S, T}(p::StdPair) where {S, T} = Pair{S, T}(first(p), last(p))
 Base.convert(::Type{<:Pair{T,S}}, p::StdPair) where {T,S} =
-    Pair(T(first(p)), S(last(p)))
+    Pair{T,S}(T(first(p)), S(last(p)))
 
 Base.length(p::StdPair) = 2
 Base.eltype(p::StdPair{T,T}) where T = T
