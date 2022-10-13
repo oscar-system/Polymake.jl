fee94c40bee40424fdfb1f2e7063494b72e72c80
Benjamin Lorenz <lorenz@math.tu-berlin.de>
2022-10-13 19:24:26 +0200

    avoid some duplication

--- src/perlobj.jl
+++ src/perlobj.jl
@@ -48,23 +48,7 @@ end
 
 Base.setproperty!(obj::BigObject, prop::String, val) = setproperty!(obj, Symbol(prop), val)
 
-function give(obj::BigObject, prop::String)
-    return_obj = try
-        disable_sigint() do
-            internal_give(obj, prop)
-        end
-    catch ex
-        ex isa ErrorException && throw(PolymakeError(ex.msg))
-        if (ex isa InterruptException)
-            @warn """Interrupting polymake is not safe.
-            SIGINT is disabled while waiting for polymake to finish its computations."""
-        end
-        rethrow(ex)
-    end
-    return convert_from_property_value(return_obj)
-end
-
-function give(obj::BigObject, prop::Symbol)
+function give(obj::BigObject, prop::Union{Symbol,String})
     return_obj = try
         disable_sigint() do
             internal_give(obj, prop)
