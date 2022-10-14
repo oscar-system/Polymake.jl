f5d9c949cf1d302cb40be128ac1b455525426ba6
alexej-jordan <58329349+alexej-jordan@users.noreply.github.com>
2022-10-14 09:57:35 +0200

    Improve SparseVector (scalar multiplication) (#405)
    Co-authored-by: Benjamin Lorenz <benlorenz@users.noreply.github.com>

--- src/sparsevector.jl
+++ src/sparsevector.jl
@@ -52,6 +52,28 @@ SparseArrays.nonzeroinds(vec::SparseVector) = Int[to_one_based_indexing(i) for i
 
 SparseArrays.nonzeros(vec::SparseVector{T}) where T = findnz(vec)[2]
 
+function Base.:*(s::Number, vec::SparseVector{T}) where T
+    JT = to_jl_type(T)
+    P = promote_to_pm_type(SparseVector, promote_type(JT, typeof(s)))
+    if P == JT
+        return convert(JT, s) * vec
+    else
+        return convert(P, s) * SparseVector{P}(vec)
+    end
+end
+
+Base.:*(vec::SparseVector, s::Number) = s * vec
+
+function Base.:/(vec::SparseVector{T}, s::Number) where T
+    JT = to_jl_type(T)
+    P = promote_to_pm_type(SparseVector, promote_type(JT, typeof(s)))
+    if P == JT
+        return vec / convert(JT, s)
+    else
+        return SparseVector{P}(vec) / convert(P, s)
+    end
+end
+
 # implementation of SparseVector{Bool} with Int64 length using a polymake Set
 struct SparseVectorBool <: SparseVector{Bool}
     l::Int64
