c37e5fa58467d996ffd690f771f4993b2705c645
Benjamin Lorenz <lorenz@math.tu-berlin.de>
2023-05-24 14:18:15 +0200

    aqua: add proper comments regarding disabled checks

--- test/Aqua.jl
+++ test/Aqua.jl
@@ -3,13 +3,17 @@ using Aqua
 @testset "Aqua.jl" begin
    Aqua.test_all(
       Polymake;
-      ambiguities=false,               # TODO: fix ambiguities
+      # this check is disabled as it returns thousands of ambiguities from
+      # libcxxwrap defined interfaces where we can't really do anything about
+      # (at least until someone has time to work on CxxWrap for this)
+      ambiguities=false,
       unbound_args=true,
       undefined_exports=true,
       project_extras=true,
       stale_deps=true,
       deps_compat=true,
       project_toml_formatting=true,
-      piracy=false                     # TODO: fix piracy
+      # this is also disabled to to the way libcxxwrap defines c++ function mappings
+      piracy=false,
    )
 end
