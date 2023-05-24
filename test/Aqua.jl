1c47843fae9f6f9d7af20cfe7c11d824c5eb465a
Lars Göttgens <lars.goettgens@rwth-aachen.de>
2023-05-24 11:56:19 +0200

    Add `Aqua.jl` for some static testing

---
+++ test/Aqua.jl
@@ -0,0 +1,15 @@
+using Aqua
+
+@testset "Aqua.jl" begin
+   Aqua.test_all(
+      Polymake;
+      ambiguities=false,               # TODO: fix ambiguities
+      unbound_args=true,
+      undefined_exports=true,
+      project_extras=true,
+      stale_deps=true,
+      deps_compat=false,               # TODO: fix
+      project_toml_formatting=true,
+      piracy=false                     # TODO: fix piracy
+   )
+end
