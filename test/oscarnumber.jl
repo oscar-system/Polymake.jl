62e470eae345c38ae763196ecb022a284413e25a
Benjamin Lorenz <lorenz@math.tu-berlin.de>
2023-09-27 10:52:09 +0200

    test float conversion

--- test/oscarnumber.jl
+++ test/oscarnumber.jl
@@ -112,6 +112,9 @@
         if _with_oscar
             @test M * A2 == A2 * M == Polymake.OscarNumber(a1*a2 + 15 + 7*a2)
             @test Polymake.OscarNumber(5) // A2 == Polymake.OscarNumber(a2^2)
+            @test Polymake.common.convert_to{Float64}(A2) isa Float64
+            fa2 = Polymake.common.convert_to{Float64}(A2)
+            @test isapprox(fa2, 1.71; rtol=0.001)
         end
     end
 
