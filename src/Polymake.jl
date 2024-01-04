fd7159554edb37a9a4196285d5fb11a47d4d9b7f
Benjamin Lorenz <lorenz@math.tu-berlin.de>
2024-01-04 11:23:13 +0100

    avoid downloads timer hang during precompilation
    by setting a default downloader with shorter grace time

--- src/Polymake.jl
+++ src/Polymake.jl
@@ -20,6 +20,7 @@ import Base: ==, <, <=, *, -, +, //, ^, div, rem, one, zero,
     union, union!
 
 import Pkg
+import Downloads
 import JSON
 
 using SparseArrays
@@ -181,8 +182,12 @@ function __init__()
     # to avoid conflicts between symlinks and directories we switch to a new depstree folder name
     polymake_deps_tree = @get_scratch!("$(scratch_key)_depstree_v2")
 
+    # work around long Downloads.jl timer
+    Downloads.default_downloader!(Downloads.Downloader(grace=0.01))
     # we run this on every init to make sure all artifacts still exist
     prepare_deps_tree(polymake_deps_tree)
+    # restore default
+    Downloads.default_downloader!()
 
     polymake_user_dir = @get_scratch!("$(scratch_key)_userdir")
 
