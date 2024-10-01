1f3ec5afc2d9d065cc2617a1e8823a6ded581794
Benjamin Lorenz <lorenz@math.tu-berlin.de>
2024-10-01 13:12:35 +0200

    allow more versions

--- src/Polymake.jl
+++ src/Polymake.jl
@@ -42,7 +42,7 @@ import TOPCOM_jll
 using libpolymake_julia_jll
 using polymake_oscarnumber_jll
 
-const jlpolymake_version_range = (v"0.12.0",  v"0.13")
+const jlpolymake_version_range = (v"0.12.0",  v"0.14")
 
 struct PolymakeError <: Exception
     msg
